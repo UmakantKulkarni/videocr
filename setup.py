614853a9418a3320c85fd0811c1b6111dac1aa4c
Umakant Kulkarni <UmakantKulkarni@users.noreply.github.com>
2023-01-16 07:26:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Yi Ge',
     author_email='me@yige.ch',
     url='https://github.com/UmakantKulkarni/videocr',
-    download_url='https://github.com/UmakantKulkarni/videocr/archive/v1.0.zip',
+    download_url='https://github.com/UmakantKulkarni/videocr/archive/refs/tags/v1.0.tar.gz',
     install_requires=[
         'fuzzywuzzy>=0.17',
         'python-Levenshtein>=0.12',
